a773aa4359bfadc4f09dc6caf6be398bb9ea84cf
aunghtet-star <aunghtetthu172@gmail.com>
2025-10-21 23:18:18 +0630

    fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,4 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
-    // Remove .sourceMaps() for production
-    // Add version() for cache busting
-    .version();
+    .sourceMaps();
